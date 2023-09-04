fd2dc470b6b427cdbf49d5360e65f17a5fd96ca3
Filippo Lucchelli <filippo.lucchelli@gmail.com>
2023-09-04 15:03:30 +0200

    commented useless stuff

--- processing.py
+++ processing.py
@@ -22,7 +22,7 @@ def undistort(path, meta):
     #Get metadata from the picture
     BlackLevel=meta.get_item("EXIF:BlackLevel")
     VignettingData_string=meta.get_item("XMP:VignettingData")
-    VD=[float(VignettingData_string.split(',')[i]) for i in range(6)]
+    #VD=[float(VignettingData_string.split(',')[i]) for i in range(6)]
     CenterX=meta.get_item("XMP:CalibratedOpticalCenterX")
     CenterY=meta.get_item("XMP:CalibratedOpticalCenterY")
     DewarpData_string=meta.get_item("XMP:DewarpData")
@@ -30,9 +30,9 @@ def undistort(path, meta):
     fx, fy, cx, cy, k1, k2, p1, p2, k3 = DD[0], DD[1], DD[2], DD[3], DD[4], DD[5], DD[6], DD[7], DD[8]
     CameraMatrix=np.array([[fx, 0, CenterX+cx], [0, fy, CenterY+cy], [0, 0, 1]])
     DistCoeff=np.array([k1, k2, p1, p2, k3])
-    ValGain=meta.get_item("XMP:SensorGain")
-    ValETime=meta.get_item("XMP:ExposureTime")/1e6
-    PCam=meta.get_item("XMP:SensorGainAdjustment")
+    #ValGain=meta.get_item("XMP:SensorGain")
+    #ValETime=meta.get_item("XMP:ExposureTime")/1e6
+    #PCam=meta.get_item("XMP:SensorGainAdjustment")
 
     #norm_img=(raw_img-BlackLevel)/65535
     undistorted_img=cv2.undistort(raw_img, CameraMatrix, DistCoeff)
